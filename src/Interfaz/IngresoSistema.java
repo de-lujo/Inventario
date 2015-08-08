388b500ae11f25de6afda8f8292f6effaebf348a
de-lujo <hip-hop_de-lujo@hotmail.com>
2015-08-08 20:54:08 -0300

    archivo no utilizado
    archivo no utilizado

--- src/Interfaz/IngresoSistema.java
+++ src/Interfaz/IngresoSistema.java
@@ -71,7 +71,7 @@ public class IngresoSistema extends javax.swing.JFrame {
             if(cantidad>1){
                 this.setVisible(false);
                     JOptionPane.showMessageDialog(null, "Bienvenido Vendedor");
-                     VentanaAdmin ingreso = new VentanaAdmin();
+                    VentanaAdmin ingreso = new VentanaAdmin();
                     ingreso.setVisible(true);
                     ingreso.pack();
             } 
